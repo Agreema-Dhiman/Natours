1f6a07a990e505f52920a888680a309261e25293
Agreema-Dhiman <agreemadhiman@gmail.com>
2024-05-27 16:30:00 +0530

    port modification

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose
 
 /////////////////////////SERVER/////////////////////
 ///////////////
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
